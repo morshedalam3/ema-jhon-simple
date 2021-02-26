6f9cb377b957e16ba4e54bd33de4fb1cd0520026
morshedalam3 <morshedhasan1969@gmail.com>
2021-02-27 02:32:52 +0600

    shopping added done

--- src/componant/Products/Product.js
+++ src/componant/Products/Product.js
@@ -1,11 +1,27 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCoffee, faShoppingCart } from '@fortawesome/free-solid-svg-icons'
+import './Product.css';
 
 const Product = (props) => {
+    const {name, img, seller, price, stock} = props.product;
     return (
+        <div className="product">
+            <div>
+                <img src={img} alt=""/>
+            </div>
         <div>
-            <h4>{props.product.name}</h4>
-          
+            <h4 className="product-name">{name}</h4>
+            <br/>
+            <p><small>by: {seller}</small></p>
+            <p>${price}</p>
+            <br/>
+            <p><small>only{stock} left in stock-order soon</small></p>
+            <button onClick={() => props.handleAddProduct(props.product)} className="button-main"><FontAwesomeIcon icon={faShoppingCart} />add to cart</button>
+
         </div>
+        </div>
+        
     );
 };
 
--- src/componant/Shop/Shop.js
+++ src/componant/Shop/Shop.js
@@ -1,22 +1,31 @@
 import React, { useState } from 'react';
 import fakeData from '../../fakeData';
+import Cart from '../Cart/Cart';
 import Product from '../Products/Product';
 import './Shop.css';
 const Shop = () => {
     const firstTen = fakeData.slice(0,10);
     const [products, setProduct] = useState(firstTen)
+    const [cart, setCart] = useState([])
+    const handleAddProduct = (product) =>{
+        const newCart = [...cart, product];
+        setCart(newCart);
+    }
     
     return (
         <div className="shop-container">
             <div className="product-container">
              
                  {
-                    products.map(pd => <Product product={pd}></Product> )
+                    products.map(pd => <Product 
+                        handleAddProduct = {handleAddProduct}
+                        product={pd}
+                    ></Product> )
                  }
               
             </div>
             <div className="card-container">
-                <p>this is card</p>
+               <Cart cart={cart}></Cart>
             </div>
         </div>
     );
